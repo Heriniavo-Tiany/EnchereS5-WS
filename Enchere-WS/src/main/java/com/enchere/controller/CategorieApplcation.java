e65f65c0e6860f7d1cf372a761ab6124a6c5601a
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-01-18 22:43:53 +0300

    Update CategorieApplcation.java

--- Enchere-WS/src/main/java/com/enchere/controller/CategorieApplcation.java
+++ Enchere-WS/src/main/java/com/enchere/controller/CategorieApplcation.java
@@ -14,6 +14,7 @@ public class CategorieApplcation {
     JdbcTemplate jdbc;
 
     @GetMapping("/CategoriesEnchere")
+    @CrossOrigin
     public List<Categorie> getCategories() {
         return jdbc.query(
                 "SELECT * FROM categoriesenchere",
@@ -36,14 +37,26 @@ public class CategorieApplcation {
         return jdbc.query(
                 query,
                 (rs, rowNum) -> new Categorie(rs.getString("idcategorie"), rs.getString("nom")));
-
     }
 
-    @PostMapping("/updateCatEnchere")
+    @RequestMapping(value = "/updateCatEnchere", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    @CrossOrigin
     public void update(@RequestParam(value = "id") String id, @RequestParam(value = "nom") String nom) {
         String query = String.format("update categoriesenchere set nom='%s' where idcategorie='%s'",
                 nom, id);
         System.out.println(query);
         jdbc.batchUpdate(query);
+    }
+
+    @DeleteMapping("/categories/{id}")
+    @CrossOrigin
+    public Object deleteCategorie(@PathVariable String id) throws Exception {
+        String query = String.format("DELETE FROM categoriesenchere WHERE idcategorie='%s'", id);
+
+        if (jdbc.batchUpdate(query)[0] != 0)
+            return "Category " + id + " deleted  successfully";
+        else
+            throw new Exception("Error on delete category");
     }
 }
