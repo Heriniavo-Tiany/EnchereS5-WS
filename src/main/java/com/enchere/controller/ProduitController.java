ddad771e244d8610d9dfa061eb4c68a87f8ff233
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2023-02-03 00:56:49 +0300

    insert new Produit

--- src/main/java/com/enchere/controller/ProduitController.java
+++ src/main/java/com/enchere/controller/ProduitController.java
@@ -1,12 +1,12 @@
 package com.enchere.controller;
 
+import com.enchere.model.Error;
+import com.enchere.model.Produit;
 import com.enchere.model.Rechargement;
+import com.enchere.model.User;
 import com.enchere.mongoDB.ProduitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -26,4 +26,23 @@ public class ProduitController {
     public Object getProduitById(@PathVariable String id) {
         return produitRepository.findById(id);
     }
+
+
+    @RequestMapping(value = "/produits", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    @CrossOrigin
+    public Object insertProduit(
+            @RequestParam(value = "idproduit") String idproduit,
+            @RequestParam(value = "nom") String nom,
+            @RequestParam(value = "categorie") String categorie,
+            @RequestParam(value = "description") String description
+    ) {
+        Produit produit = new Produit();
+        produit.setIdproduit(idproduit);
+        produit.setNom(nom);
+        produit.setCategorie(categorie);
+        produit.setDescription(description);
+        return produitRepository.insert(produit);
+    }
+
 }
